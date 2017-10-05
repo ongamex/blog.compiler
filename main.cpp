400bf1dcf5144a814ec367f630ddfabfc1628e31
ongamex <ongamex@windowslive.com>
2017-10-05 21:02:14 +0300

    - inital expr. parser

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <assert.h>
 
 enum TokenType
@@ -90,6 +91,14 @@ struct Lexer
 			m_ptr++;
 			return Token(tokenType_slash);
 		}
+		else if(*m_ptr == '+') {
+			m_ptr++;
+			return Token(tokenType_plus);
+		}
+		else if(*m_ptr == '-') {
+			m_ptr++;
+			return Token(tokenType_minus);
+		}
 		else if(*m_ptr == '(') {
 			m_ptr++;
 			return Token(tokenType_lparen);
@@ -98,39 +107,10 @@ struct Lexer
 			m_ptr++;
 			return Token(tokenType_rparen);
 		}
-		else if(*m_ptr == '+' || *m_ptr == '-' || isdigit(*m_ptr))
+		else if(isdigit(*m_ptr))
 		{
-			float numberSign = 1.f;
-
-			// This could be a number or just a sign. To check that we need to look one symbol ahead.
-			if(*m_ptr == '+' || *m_ptr == '-')
-			{
-				if(isdigit(*(m_ptr+1)) == false)
-				{
-					// This is not a number, it must be just a sign.
-					if(*m_ptr == '+') { 
-						m_ptr++;
-						return Token(tokenType_plus);
-					}
-					else if(*m_ptr == '-') {
-						m_ptr++;
-						return Token(tokenType_minus);
-					}
-					else 
-					{
-						// Should never happen.
-						assert(false);
-						return Token();
-					}
-				}
-				else
-				{
-					if(*m_ptr == '-') numberSign = -1.f;
-					m_ptr++;
-				}
-			}
-
 			// This should be a number.
+			// TODO: Floating point numbers.
 			float numberAccum = 0;
 			while(isdigit(*m_ptr)){
 				int digit = *m_ptr - '0';
@@ -140,7 +120,7 @@ struct Lexer
 
 			Token token;
 			token.type = tokenType_number;
-			token.numberData = numberAccum * numberSign;
+			token.numberData = numberAccum;
 
 			return token;
 		}
@@ -154,31 +134,191 @@ struct Lexer
 	const char* m_ptr = nullptr;
 };
 
+enum AstNodeType
+{
+	astNodeType_invalid,
+	astNodeType_number,
+	astNodeType_identifier,
+	astNodeType_binop,
+};
+
+struct AstNode
+{
+	AstNode(AstNodeType const type = astNodeType_invalid) :
+		type(type)
+	{}
+	virtual ~AstNode() = default;
+
+	AstNodeType type;
+};
+
+struct AstNumber : public AstNode
+{
+	AstNumber(float const value = NAN) 
+		: AstNode(astNodeType_number)
+		, value(value)
+	{}
+
+	float value = NAN;
+};
+
+struct AstIdentifier : public AstNode
+{
+	AstIdentifier(std::string identifier) 
+		: AstNode(astNodeType_identifier)
+		, identifier(identifier)
+	{}
+
+	std::string identifier;
+};
+
+struct AstBinOp : public AstNode
+{
+	AstBinOp(char op, AstNode* left, AstNode* right) 
+		: AstNode(astNodeType_binop)
+		, op(op)
+		, left(left)
+		, right(right)
+	{}
+
+	AstNode* left;
+	AstNode* right;
+	char op; // + i * /
+};
+
+struct Parser
+{
+
+	AstNode* root = nullptr;
+	const Token* m_token = nullptr;
+
+	void parse()
+	{
+		root = parse_exporession();
+	}
+
+	AstNode* parse_exporession()
+	{
+		return parse_expression2();
+	}
+
+	AstNode* parse_exporession0()
+	{
+		if(m_token->type == tokenType_number)
+		{
+			AstNode* const result =  new AstNumber(m_token->numberData);
+			matchAny();
+			return result;
+		}
+		else if(m_token->type == tokenType_identifier)
+		{
+			AstNode* const result =  new AstIdentifier(m_token->strData);
+			matchAny();
+			return result;
+		}
+		else if(m_token->type == tokenType_lparen)
+		{
+			matchAny();
+			AstNode* const result =  parse_exporession();
+			match(tokenType_rparen);
+			return result;
+		}
+
+		assert(false);
+		return nullptr;
+	}
+
+	AstNode* parse_expression1()
+	{
+		AstNode* left = parse_exporession0();
+
+		if(m_token->type == tokenType_asterisk)
+		{
+			matchAny();
+			AstNode* retval = new AstBinOp('*', left, parse_exporession());
+			return retval;
+		}
+		else if(m_token->type == tokenType_slash)
+		{
+			matchAny();
+			AstNode* retval = new AstBinOp('/', left, parse_exporession());
+			return retval;
+		}
+
+		return left;
+	}
+
+	AstNode* parse_expression2()
+	{
+		AstNode* left = parse_expression1();
+
+		if(m_token->type == tokenType_plus)
+		{
+			matchAny();
+			AstNode* retval = new AstBinOp('+', left, parse_exporession());
+			return retval;
+		}
+		else if(m_token->type == tokenType_minus)
+		{
+			matchAny();
+			AstNode* retval = new AstBinOp('-', left, parse_exporession());
+			return retval;
+		}
+
+		return left;
+	}
+
+	void matchAny()
+	{
+		++m_token;
+	}
+
+	void match(TokenType const type)
+	{
+		if(m_token->type != type)
+		{
+			// A compilation error here!
+			assert(false);
+			return;
+		}
+		++m_token;
+	}
+};
+
 
 int main()
 {
 	const char* const testCode = R"(
-ident0 = 334;
-ident0 = -1*ident1 + ident2;
-ident3=-500*+1/3*-3;
+ident0 + 3*2
 )";
 
 	Lexer lexer(testCode);
 
+	std ::vector<Token> tokens;
+
 	while(true)
 	{
 		const Token tok = lexer.getNextToken();
+		tokens.push_back(tok);
 	
-		if(tok.type == tokenType_number) printf("NUMBER %f;\n", tok.numberData);
-		if(tok.type == tokenType_identifier) printf("IDENT %s;\n", tok.strData.c_str());
+		if(tok.type == tokenType_number) printf("%f ", tok.numberData);
+		if(tok.type == tokenType_identifier) printf("IDENT(%s) ", tok.strData.c_str());
 		if(tok.type == tokenType_semicolon) printf("; \n");
-		if(tok.type == tokenType_assign) printf("= \n");
-		if(tok.type == tokenType_plus) printf("+ \n");
-		if(tok.type == tokenType_minus) printf("- \n");
-		if(tok.type == tokenType_asterisk) printf("* \n");
-		if(tok.type == tokenType_slash) printf("/ \n");
-		if(tok.type == tokenType_none) { printf("NONE;\n "); break; }
+		if(tok.type == tokenType_assign) printf("= ");
+		if(tok.type == tokenType_plus) printf("+ ");
+		if(tok.type == tokenType_minus) printf("- ");
+		if(tok.type == tokenType_asterisk) printf("* ");
+		if(tok.type == tokenType_slash) printf("/ ");
+		if(tok.type == tokenType_lparen) printf("(");
+		if(tok.type == tokenType_rparen) printf(") ");
+		if(tok.type == tokenType_none) { printf("\nNONE;\n "); break; }
 	}
+
+	Parser p;
+	p.m_token = tokens.data();
+
+	p.parse();
+	AstNode* root = p.root;
 
 
 	return 0;
